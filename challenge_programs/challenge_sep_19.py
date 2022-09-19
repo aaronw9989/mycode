3624c42539ad5f2fbb80222a4c74cd18fbb5d184
Aaron Williams <aaronw9989@users.noreply.github.com>
2022-09-19 22:08:41 +0000

    Completed challenge

---
+++ challenge_programs/challenge_sep_19.py
@@ -0,0 +1,12 @@
+#!/use/bin/env python3
+
+def main():
+
+    user_name = input("Please enter a user name: ")
+
+    day_of_week = input("Please enter what day of the week it is? ") 
+
+    print(f"Hello, {user_name}! Happy {day_of_week}!")
+
+
+main()
