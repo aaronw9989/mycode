b6b7945de53c3fbf2ca657832383c60292f65ef7
Aaron Williams <aaronw9989@users.noreply.github.com>
2022-09-20 17:47:10 +0000

    mixed lists

--- challenge_programs/challenge_sep_19.py
+++ challenge_programs/challenge_sep_19.py
@@ -2,11 +2,16 @@
 
 def main():
 
+    # Get the name of the user
     user_name = input("Please enter a user name: ")
 
+    # Get the day of the week
     day_of_week = input("Please enter what day of the week it is? ") 
 
+    # Output the user name and day of the week
     print(f"Hello, {user_name}! Happy {day_of_week}!")
 
 
-main()
+# call the main function
+if __name__ == "__main__":
+    main()
