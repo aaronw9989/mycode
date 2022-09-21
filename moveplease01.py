7a8acac89ad81c5780156abcfccdfc788b4212c3
Aaron Williams <aaronw9989@users.noreply.github.com>
2022-09-21 18:01:59 +0000

    renaming and moving files

--- moveplease01.py
+++ moveplease01.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+""" A simple script to move files into ceph_storage """
+
 
 import shutil
 import os
@@ -11,10 +13,13 @@ def main():
     # copy fileA to fileB
     shutil.move('raynor.obj', 'ceph_storage/')
 
-    
+    # prompt user for a new name for the kerrigan.obj file
+    xname = input('What is the new name for kerrigan.obj? ')
+
+    # rename the current kerrigan.obj file
+    shutil.move('ceph_storage/kerrigan.obj', 'ceph_storage/' + xname)
 
 
-
-
+# this calls our main function
 if __name__ == "__main__":
     main()
