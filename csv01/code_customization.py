3cf0438b03c075953b4140754163df9ee63ebf1f
Aaron Williams <aaronw9989@users.noreply.github.com>
2022-09-27 20:11:51 +0000

    csv and pandas

---
+++ csv01/code_customization.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+import csv
+
+def main():
+    with open('superbirthday.csv', mode='r') as csv_file:
+    with open('regularbirthday.csv', mode='w') as csv_custom:
+                    
+        csv_reader = csv.DictReader(csv_file)
+        line_count = 0
+
+            csv_reader2 = csv.DictReader(csv_customer)
+
+
+        for row in csv_reader:
+            if line_count == 0:
+                # print(f'Column names are {", ".join(row)}') # python3.6 way
+                                                              ## to do things
+                print('Column names are {}'.format(", ".join(row)))
+                    print('Column names are {}'.format(", ".join(row)), file='regularbirthday.csv')
+                line_count += 1
+            # print(f'\t{row["name"]} aka {row["heroname"]} was born in {row["birthday month"]}.')
+            # above is the python3.6+ way to do things
+            print('\t{} aka {} was born in {}.'.format(row["name"],row["heroname"],row["birthday month"]))
+            line_count += 1
+    # print(f'Processed {line_count} lines.') # python3.6 way to do things
+    print('Processed {} lines.'.format(line_count))
+if __name__ == "__main__":
+    main()
+
